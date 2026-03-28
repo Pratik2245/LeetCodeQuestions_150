4cc3fb088bbff28f03e122c82752fca1d9b07124
pratik <paithankarpratik3@gmail.com>
2026-03-28 11:30:44 +0530

    LT_148SortList done

---
+++ src/LT_148SortList.java
@@ -0,0 +1,51 @@
+public class LT_148SortList {
+    public static void main(String[] args) {
+
+    }
+    public ListNode sortList(ListNode head) {
+        if(head==null || head.next==null)
+            return head;
+        ListNode mid=getMid(head);
+        ListNode right=mid.next;
+        mid.next=null;
+
+        ListNode left=sortList(head);
+        right=sortList(right);
+        return merge(left,right);
+    }
+    public static ListNode getMid(ListNode head){
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+    public static ListNode merge(ListNode l1,ListNode l2){
+        ListNode dummy=new ListNode(0);
+        ListNode curr=dummy;
+        while(l1!=null && l2!=null){
+            if(l1.val>l2.val){
+                curr.next=l2;
+                l2=l2.next;
+            }else{
+                curr.next=l1;
+                l1=l1.next;
+            }
+            curr=curr.next;
+        }
+        if (l1 != null) curr.next = l1;
+        if (l2 != null) curr.next = l2;
+        return dummy.next;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
---
+++ src/LT_5LongestPalindromicSubString.java
@@ -0,0 +1,4 @@
+package PACKAGE_NAME;
+
+public class LT_5LongestPalindromicSubString {
+}
